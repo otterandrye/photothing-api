c002aa59b4b42254ba9a4e6ccb308ea89662d548
Nathan Merritt <gnm@the-merritts.net>
2018-07-11 13:58:11 -0400

    try and find cargo during build

--- utils/pre_release.sh
+++ utils/pre_release.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-set -e
+
+# cargo might not be on the path on the dyno, try and add it
+[[ !$(type -P cargo) ]] || PATH="$CACHE_DIR/cargo/bin:$PATH"
 
 cargo install diesel_cli --no-default-features --features postgres
 diesel migration run
-
