ca8e2db905284c32802f081aad7330ad5069cd66
Nathan Merritt <gnm@the-merritts.net>
2018-07-11 14:12:50 -0400

    try again

--- utils/pre_release.sh
+++ utils/pre_release.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 # cargo might not be on the path on the dyno, try and add it
-[[ !$(type -P cargo) ]] || PATH="$CACHE_DIR/cargo/bin:$PATH"
+if ! [[ $(type -P cargo) ]]; then
+  echo "Trying to find Cargo. Path before: $PATH"
+  PATH="$CACHE_DIR/cargo/bin:$PATH"
+  echo "Path after: $PATH"
+  echo "Cargo: $(which cargo)"
+fi
 
 cargo install diesel_cli --no-default-features --features postgres
 diesel migration run
