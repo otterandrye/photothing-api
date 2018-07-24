b822c788ad1b08ebe95ee11ff9682519b363613c
Nathan Merritt <gnm@the-merritts.net>
2018-07-24 19:08:59 -0400

    Edit subscription end date

--- src/db/user.rs
+++ src/db/user.rs
@@ -32,6 +32,13 @@ impl User {
             Ok(Some(results.swap_remove(0)))
         }
     }
+
+    pub fn edit_subscription(self, db: &PgConnection, expiration_date: Option<NaiveDate>) -> Result<User, Error> {
+        use db::schema::users::dsl::*;
+        diesel::update(&self)
+            .set(subscription_expires.eq(expiration_date))
+            .get_result(db)
+    }
 }
 
 #[derive(Insertable)]
@@ -59,5 +66,29 @@ impl NewUser {
         use db::schema::users::dsl::*;
         let created = diesel::insert_into(users).values(&self).get_result(db)?;
         Ok(created)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use dotenv;
+
+    use db::{DbConn, init_db_pool};
+    use util::HashedPassword;
+    use super::*;
+
+    #[test]
+    fn user_crud() {
+        dotenv::dotenv().ok();
+        let pool = init_db_pool();
+        let db = DbConn(pool.get().expect("couldn't connect to db"));
+        let user = NewUser::new(String::from("foo"), HashedPassword(String::from("foobar")));
+        let user = user.insert(&db).expect("couldn't make user");
+        assert!(user.subscription_expires.is_none(), "new user has subscription by default");
+        assert_eq!(user.email, "foo");
+
+        let date = NaiveDate::from_ymd(2015, 3, 14);
+        let updated = user.edit_subscription(&db, Some(date)).expect("update error'd");
+        assert_eq!(Some(date), updated.subscription_expires, "date update failed");
     }
 }
